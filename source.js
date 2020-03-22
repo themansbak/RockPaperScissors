7f87abaefed8e1fcd5bf1c477d43adb72588f15f
aman <man.alex1995@gmail.com>
2020-03-21 23:06:24 -0700

    Finishing touches

--- source.js
+++ source.js
@@ -1,6 +1,3 @@
-/*
-Every time the player clicks a selection, the compute play will be called
-*/
 const buttons = document.querySelectorAll('.img-icon');
 buttons.forEach(button => button.addEventListener('click', computePlay));
 
@@ -9,18 +6,17 @@ reset.addEventListener('click', resetScore);
 
 var player = 0;
 var computer = 0;
+var totalGames = 0;
 
 function resetScore() {
   player = 0;
   computer = 0;
   updateScore();
 }
-
 function updateScore() {
   const scoreBoard = document.querySelector('#score');
   scoreBoard.textContent = 'Computer: ' + computer + ' || Player: ' + player;
 }
-
 function computerSelection() {
   switch(Math.floor(Math.random()*3)) {
     case 0:
@@ -31,7 +27,6 @@ function computerSelection() {
       return 'scissors';
   }
 }
-
 function computeWinner(pChoice, cChoice) {
   if (pChoice == cChoice) return 'tie';
   else if (pChoice == 'rock') {
@@ -47,14 +42,27 @@ function computeWinner(pChoice, cChoice) {
     else if (cChoice == 'paper') return 'player';
   }
 }
-
+function checkTotalWinner() {
+  if (totalGames%5===0) {
+    const result = document.querySelector('.p-result');
+    if (player > computer) result.textContent = 'Player won best ouf of 5!';
+    else result.textContent = 'Computer won: best out of 5!'
+    resetScore();
+  }
+}
+function updateWinner(winner) {
+  const result = document.querySelector('.p-result');
+  if (winner === 'tie') result.textContent = 'Result: tie!';
+  else result.textContent = 'Result: ' + winner + ' won!';
+}
 function computePlay(e) {
   const compSelection = computerSelection();
   const playerSelection = e.target.id;
-  console.log(compSelection, ' ', playerSelection);
-  // alert(e.target.id);
   winner = computeWinner(playerSelection, compSelection);
+  updateWinner(winner);
   if (winner == 'player') player += 1;
   else computer += 1;
+  totalGames += 1;
   updateScore();
+  checkTotalWinner();
 }
